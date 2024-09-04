6495242e7c8225a9cfec7edeecfbc4e843159460
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-04 23:42:18 +0700

    flex box in react native

--- components/flex.box.tsx
+++ components/flex.box.tsx
@@ -2,33 +2,30 @@ import { StyleSheet, Text, View } from "react-native";
 const styles = StyleSheet.create({
   container: {
     marginTop: 40,
-    flexDirection: "row",
+    // flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
+    // alignItems: "center",
     borderWidth: 1,
     borderColor: "green",
+    // flex: 1,
   },
   item1: {
     backgroundColor: "green",
     padding: 30,
-    width: 50,
-    height: 50,
+    flex: 2,
   },
   item2: {
     backgroundColor: "orange",
     padding: 30,
-    height: 100,
   },
   item3: {
     backgroundColor: "yellowgreen",
     padding: 30,
-    // height: 150,
     flex: 1,
   },
   item4: {
     backgroundColor: "red",
     padding: 30,
-    width: 50,
   },
 });
 function FlexBox() {
