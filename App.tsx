9b2c367b9c2d939931c61ce62808cb6d827b197e
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-05 00:08:11 +0700

    use icon in react native, add icons for todo app

--- App.tsx
+++ App.tsx
@@ -13,26 +13,23 @@ import {
   TouchableWithoutFeedback,
   View,
 } from "react-native";
-import FlexBox from "./components/flex.box";
 
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 // define kiểu đối tượng
 interface ITodo {
   id: number;
   name: string;
 }
-
 export default function App() {
   // return jsx
   // hooks in react native, and use typescript
   // input todo
   const [todo, setTodo] = useState("");
   const [listTodo, setListTodo] = useState<ITodo[]>([]);
-
   // random number
   function randomInteger(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
   const handleAddTodo = () => {
     if (!todo) {
       // if todo is empty
@@ -56,20 +53,52 @@ export default function App() {
     setTodo("");
   };
 
-  const deleteTodo = (id: number) => {
-    // lọc ra những todo có id khác với id truyền vào
-    // giữ lại những todo có id khác với id truyền vào
-    const newTodo = listTodo.filter((item) => item.id !== id);
-
-    // update list to
-    setListTodo(newTodo);
-  };
-
   return (
     // hide keyboard if click outside the textinput
-    <View>
-      <FlexBox />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        {/* header */}
+        <Text style={styles.header}>Todo App</Text>
+        {/* marginVertical: marginTop and marginBottom */}
+        {/* form */}
+        <View>
+          <TextInput
+            value={todo}
+            style={styles.todoInput}
+            onChangeText={(value) => setTodo(value)}
+          />
+          <Button title="Add todo" onPress={handleAddTodo} />
+        </View>
+
+        {/* list todo */}
+        <View style={styles.todo}>
+          {/* <Text>list todo: {todo}</Text> */}
+          <FlatList
+            data={listTodo}
+            renderItem={(data) => {
+              return (
+                <Pressable
+                  // onPress={() => deleteTodo(data.item.id)}
+                  style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
+                >
+                  <View style={styles.groupTodo}>
+                    <Text style={styles.todoItem}>{data.item.name}</Text>
+                    <MaterialIcons
+                      name="restore-from-trash"
+                      size={24}
+                      color="black"
+                    />
+                  </View>
+                </Pressable>
+              );
+            }}
+            keyExtractor={(item) => item.id + ""}
+          />
+
+          {/* <Text>{JSON.stringify(listTodo)}</Text> */}
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -77,20 +106,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   // css in js
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
   header: {
     fontSize: 24,
     paddingHorizontal: 20,
     backgroundColor: "yellow",
     textAlign: "center",
   },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 50,
 
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
   todoInput: {
     borderWidth: 1,
     borderColor: "green",
@@ -102,6 +131,13 @@ const styles = StyleSheet.create({
     borderColor: "black",
     padding: 10,
     marginVertical: 20,
-    marginHorizontal: 40,
+    marginHorizontal: 10,
+    flex: 1,
+  },
+  todo: { flex: 8 },
+  groupTodo: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 });
