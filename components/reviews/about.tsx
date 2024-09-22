2052f989f59dda1a813506b2ff9c6c7e5532c49c
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-22 16:39:13 +0700

    use many styles

--- components/reviews/about.tsx
+++ components/reviews/about.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { globalStyles } from "../../utils/const";
+
+const styles = StyleSheet.create({
+  about: {
+    fontSize: 30,
+  },
+});
 
 function AboutScreen() {
   return (
     <View>
-      <Text>About Screen</Text>
+      <Text style={{ color: "yellowgreen", fontWeight: "bold", fontSize: 24 }}>
+        Gộp CSS trong react native thông qua mảng []
+      </Text>
+      <Text style={[styles.about, globalStyles.globalFont]}>About Screen</Text>
     </View>
   );
 }
--- utils/const.ts
+++ utils/const.ts
@@ -1,2 +1,11 @@
+import { StyleSheet } from "react-native";
+
 export const OPENSANS_REGULAR = "OpenSans-Regular";
 
+// use for the whole app
+export const globalStyles = StyleSheet.create({
+    // use this fonts for the app
+    globalFont:{
+        fontFamily: OPENSANS_REGULAR,
+    }
+})
